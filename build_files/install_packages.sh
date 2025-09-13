6c401ce4cb544dc795a4f0666312d4d7e787a1f6
git-mann <me@hmann.dev>
2025-09-13 19:42:26 -0400

    Install sssd-common first

--- build_files/install_packages.sh
+++ build_files/install_packages.sh
@@ -10,6 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
+dnf5 install -y sssd-common
+
 dnf5 install -y \
     freeipa-client \
     sssd-passkey \
